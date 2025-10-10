8c3a074a474d8f1d7938f5aff9ab976adfb1bf1c
Josh Granek <josh@duke.edu>
2025-10-10 14:12:43 -0400

    put --account in the right location

--- podsplit_dorado_demux/main_part3__sbatch.sh
+++ podsplit_dorado_demux/main_part3__sbatch.sh
@@ -19,14 +19,14 @@ export BAM_FILTER=${RESULTS_DIR}/bam_filter_tag.txt
 printf "1\n0\n" > ${BAM_FILTER}
 #--------------------
 
-JOBID_50=$(sbatch --parsable --array=0-$FILE_COUNT --job-name=bam_filter --partition=${CPUJOB_PARTITION} --output="$LOG_DIR/%x-%A-%a.log" --error="$LOG_DIR/%x-%A-%a.log" ${SCRIPT_PATH}/simplex_filter_bams__sbatch_job.sh) --account=${CPU_ACCOUNT} 
+JOBID_50=$(sbatch --parsable --array=0-$FILE_COUNT --job-name=bam_filter  --account=${CPU_ACCOUNT} --partition=${CPUJOB_PARTITION} --output="$LOG_DIR/%x-%A-%a.log" --error="$LOG_DIR/%x-%A-%a.log" ${SCRIPT_PATH}/simplex_filter_bams__sbatch_job.sh)
 
 export FASTQ_BAM_DIR=${CUR_BAM_DIR}
 export FASTQ_OUT_DIR=${FULL_FASTQS}
-JOBID_60=$(sbatch --parsable --array=0-$FILE_COUNT --job-name=total_fastqs --partition=${CPUJOB_PARTITION} --output="$LOG_DIR/%x-%A-%a.log" --error="$LOG_DIR/%x-%A-%a.log" ${SCRIPT_PATH}/fastq_gen__sbatch_job.sh) --account=${CPU_ACCOUNT} 
+JOBID_60=$(sbatch --parsable --array=0-$FILE_COUNT --job-name=total_fastqs  --account=${CPU_ACCOUNT} --partition=${CPUJOB_PARTITION} --output="$LOG_DIR/%x-%A-%a.log" --error="$LOG_DIR/%x-%A-%a.log" ${SCRIPT_PATH}/fastq_gen__sbatch_job.sh)
 
 
 export FASTQ_BAM_DIR=${SIMPLEX_FILTER_BAMS}
 export FASTQ_OUT_DIR=${SIMPLEX_FILTER_FASTQS}
-JOBID_70=$(sbatch --parsable --array=0-$FILE_COUNT --dependency=afterok:$JOBID_50 --job-name=simpfilt_fastqs --partition=${CPUJOB_PARTITION} --output="$LOG_DIR/%x-%A-%a.log" --error="$LOG_DIR/%x-%A-%a.log" ${SCRIPT_PATH}/fastq_gen__sbatch_job.sh) --account=${CPU_ACCOUNT} 
+JOBID_70=$(sbatch --parsable --array=0-$FILE_COUNT --dependency=afterok:$JOBID_50 --job-name=simpfilt_fastqs  --account=${CPU_ACCOUNT} --partition=${CPUJOB_PARTITION} --output="$LOG_DIR/%x-%A-%a.log" --error="$LOG_DIR/%x-%A-%a.log" ${SCRIPT_PATH}/fastq_gen__sbatch_job.sh)
 
